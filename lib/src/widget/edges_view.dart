92442b7394e5c205fc3c9e4f84768da8b7d70e23
danh-ld <danh-ld@genkisystem.com>
2026-04-22 15:22:08 +0700

    feat: add edgeLabel variable to EdgesView build method

--- lib/src/widget/edges_view.dart
+++ lib/src/widget/edges_view.dart
@@ -14,6 +14,7 @@ class EdgesView extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = InheritedConfiguration.controllerOf(context);
     final configuration = InheritedConfiguration.configurationOf(context);
+    var edgeLabel = '';
 
     return RepaintBoundary(
       child: CustomPaint(
